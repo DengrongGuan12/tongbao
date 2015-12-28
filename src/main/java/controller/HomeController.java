51edc7208fd2229a98ce7610a4a3717dd5b4fccb
DengrongGuan12 <1031212913@qq.com>
2015-12-28 22:29:53 +0800

    Initial commit

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dengrong on 2015/12/28.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("message","hello world");
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1><c:out value="${message}"/></h1>
+    <h2>Again!</h2>
+</body>
+</html>
