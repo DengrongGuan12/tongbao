4743760b2b05e38ab03af580b3adc7b4ea5c6120
DengrongGuan12 <1031212913@qq.com>
2016-01-05 16:57:40 +0800

    @service

--- src/main/java/service/impl/DriverServiceImpl.java
+++ src/main/java/service/impl/DriverServiceImpl.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import org.springframework.stereotype.Service;
 import pojo.RealNameAuthInfo;
 import pojo.TruckAuthInfo;
 import pojo.TruckInfo;
@@ -8,6 +9,7 @@ import service.DriverService;
 /**
  * Created by I322233 on 1/4/2016.
  */
+@Service
 public class DriverServiceImpl implements DriverService {
     public boolean setTruckInfo(int userId, TruckInfo truckInfo) {
         return false;
