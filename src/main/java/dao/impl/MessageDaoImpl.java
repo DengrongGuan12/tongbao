516ac83ebd04b8f0726960b272fd6052b46872fd
oliugian <565817328@qq.com>
2016-01-09 21:17:07 +0800

    New changelist

--- src/main/java/dao/impl/MessageDaoImpl.java
+++ src/main/java/dao/impl/MessageDaoImpl.java
@@ -1,9 +1,15 @@
 package dao.impl;
 
 import dao.MessageDao;
+import model.Message;
+
+import java.util.List;
 
 /**
  * Created by cg on 2016/1/9.
  */
 public class MessageDaoImpl extends BaseDaoImpl implements MessageDao {
+    public List getMyMessage(int userId) {
+        return super.getList(Message.class,"user_id",userId+"");
+    }
 }
