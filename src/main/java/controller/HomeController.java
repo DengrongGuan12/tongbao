4f151b3a21920b07d1c584060a501dd9f9f57007
DengrongGuan12 <1031212913@qq.com>
2016-01-19 19:24:34 +0800

    testEncoding

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,16 +3,25 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pojo.ModifiedNickName;
+import vo.RestResult;
 
 /**
  * Created by dengrong on 2015/12/28.
  */
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/index")
     public String home(Model model){
         model.addAttribute("message","hello world");
         return "home";
+    }
+    @RequestMapping("/testEncoding")
+    @ResponseBody
+    public RestResult testEncoding(ModifiedNickName modifiedNickName){
+        return RestResult.CreateResult(1,modifiedNickName);
     }
 
 }
